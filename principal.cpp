ca21bf857f644832ee3894060cf3c1753ed1da29
Joseph Bano <andres.jbo2003@gmail.com>
2022-07-07 11:55:44 -0500

    designe ui and cmdCalc

--- principal.cpp
+++ principal.cpp
@@ -11,5 +11,39 @@ Principal::Principal(QWidget *parent)
 Principal::~Principal()
 {
     delete ui;
+    ui->inOperation->addAction();
 }
 
+
+void Principal::on_cmdCalcular_released()
+{
+    calcular();
+}
+
+void Principal::on_actionCalcular_triggered()
+{
+    calcular();
+}
+
+void Principal::calcular()
+{
+    float num1 = ui->inNum1->value();
+    float num2 = ui->inNum2->value();
+
+    double promedio;
+    switch(ui->inOperation->currentIndex()){
+    case 0:
+        promedio=num1+num2;
+        break;
+    case 1:
+        promedio=num1-num2;
+
+    case 2:
+        promedio=num1*num2;
+    case 3:
+        promedio=num1/num2;
+    default:
+    }
+    QMessageBox::information(this, "Resultado",
+                             "El resultado es "+ QString::number(resultado))
+}
